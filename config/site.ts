dbc292e1e54cd8cc494770d4551ef98607e6c023
Tony Spiro <tspiro@tonyspiro.com>
2023-07-27 07:28:35 -0700

    edit: github link

--- config/site.ts
+++ config/site.ts
@@ -12,7 +12,7 @@ export const siteConfig = {
   ],
   links: {
     twitter: "https://twitter.com/cosmicjs",
-    github: "https://github.com/cosmicjs",
+    github: "https://github.com/cosmicjs/cosmic-next-template",
     docs: "https://www.cosmicjs.com/docs",
     login: "https://app.cosmicjs.com/login",
   },
