972af976d24cc2a55544584ea860953d2c5aae87
Tony Spiro <tspiro@tonyspiro.com>
2023-09-28 15:54:35 -0700

    edit: add site header back

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -9,7 +9,7 @@ import { ThemeToggle } from "@/components/theme-toggle"
 export function SiteHeader() {
   return (
     <header className="sticky top-0 z-40 w-full border-b bg-light-background dark:bg-dark-background">
-      {/* <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
           <nav className="flex items-center space-x-1">
@@ -42,7 +42,7 @@ export function SiteHeader() {
             <ThemeToggle />
           </nav>
         </div>
-      </div> */}
+      </div>
     </header>
   )
 }
