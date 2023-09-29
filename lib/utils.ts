c4f965c8f21bf87e51ddcc924a1977d4cedbb71a
Tony Spiro <tspiro@tonyspiro.com>
2023-09-29 15:05:53 -0700

    add: getMediaBlogFromURL

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,17 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function getMediaBlogFromURL(url: string) {
+  const response = await fetch(url)
+  const blob = await response.blob()
+  const media: any = new Blob([blob], {
+    type: "image/jpeg",
+  })
+  media.name = getImageNameFromURL(url)
+  return media
+}
+
+export function getImageNameFromURL(name: string) {
+  return name.split('https://imgix.cosmicjs.com/')[1]
+}
