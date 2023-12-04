645b707a5683f3a9c2a2c897aaf200e5f7186372
Tony Spiro <tspiro@tonyspiro.com>
2023-12-04 15:53:56 -0800

    fix: settings code indent

--- app/features/settings/page.tsx
+++ app/features/settings/page.tsx
@@ -101,105 +101,105 @@ async function Preview() {
 function Code() {
   const codeHeaderString = dedent`
     \`\`\`jsx
-    // components/header.tsx
-    import Link from "next/link";
-    import { cosmic } from "@/lib/cosmic";
-    import { NavMenu } from "@/components/nav-menu";
+      // components/header.tsx
+      import Link from "next/link";
+      import { cosmic } from "@/lib/cosmic";
+      import { NavMenu } from "@/components/nav-menu";
 
-    export default async function Header() {
-      // Header data
-      const { object: header } = await cosmic.objects
-        .findOne({
-          type: "navigation-menus",
-          slug: "header",
-        })
-        .props("metadata")
-        .depth(1);
-      
-      const { object: settings } = await cosmic.objects
-        .findOne({
-          type: "global-settings",
-          slug: "settings",
-        })
-        .props("metadata")
-        .depth(1);
+      export default async function Header() {
+        // Header data
+        const { object: header } = await cosmic.objects
+          .findOne({
+            type: "navigation-menus",
+            slug: "header",
+          })
+          .props("metadata")
+          .depth(1);
+        
+        const { object: settings } = await cosmic.objects
+          .findOne({
+            type: "global-settings",
+            slug: "settings",
+          })
+          .props("metadata")
+          .depth(1);
 
-      return (
-        <div className="my-4 flex items-center justify-between container space-x-4">
-          <Link href="/">
-            <img
-              src={\`\${settings.metadata.logo.imgix_url}?w=500&auto=format,compression\`}
-              alt={settings.metadata.company}
-              className="h-20 m-auto"
-            />
-          </Link>
-          <NavMenu items={header.metadata.items} />
-        </div>
-      );
-    }
+        return (
+          <div className="my-4 flex items-center justify-between container space-x-4">
+            <Link href="/">
+              <img
+                src={\`\${settings.metadata.logo.imgix_url}?w=500&auto=format,compression\`}
+                alt={settings.metadata.company}
+                className="h-20 m-auto"
+              />
+            </Link>
+            <NavMenu items={header.metadata.items} />
+          </div>
+        );
+      }
     \`\`\`
     `
   const codeFooterString = dedent`
     \`\`\`jsx
-    // components/footer.tsx
-    import { cosmic } from "@/lib/cosmic";
-    import { NavMenu } from "@/components/nav-menu";
+      // components/footer.tsx
+      import { cosmic } from "@/lib/cosmic";
+      import { NavMenu } from "@/components/nav-menu";
 
-    export default async function Footer() {
-      // Footer data
-      const { object: footer } = await cosmic.objects
-        .findOne({
-          type: "navigation-menus",
-          slug: "footer",
-        })
-        .props("metadata")
-        .depth(1);
+      export default async function Footer() {
+        // Footer data
+        const { object: footer } = await cosmic.objects
+          .findOne({
+            type: "navigation-menus",
+            slug: "footer",
+          })
+          .props("metadata")
+          .depth(1);
 
-      const { object: settings } = await cosmic.objects
-        .findOne({
-          type: "global-settings",
-          slug: "settings",
-        })
-        .props("metadata")
-        .depth(1);
+        const { object: settings } = await cosmic.objects
+          .findOne({
+            type: "global-settings",
+            slug: "settings",
+          })
+          .props("metadata")
+          .depth(1);
 
-      type Link = {
-        url: string;
-        company: string;
-        icon: {
-          imgix_url: string;
+        type Link = {
+          url: string;
+          company: string;
+          icon: {
+            imgix_url: string;
+          };
         };
-      };
 
-      return (
-        <div className="my-10">
-          <div className="my-8">
-            <NavMenu items={footer.metadata.items} />
-          </div>
-          <div className="mb-8 flex gap-x-8 justify-center">
-            {settings.metadata.links.map((link: Link) => {
-              return (
-                <a href={link.url} key={link.url} target="_blank" rel="noreferrer">
-                  <img
-                    className="h-[26px]"
-                    src={\`\${link.icon.imgix_url}?w=500&auto=format,compression\`}
-                    alt={link.company}
-                  />
-                </a>
-              );
-            })}
-          </div>
-          <div className="flex gap-x-8 justify-center">
-            <div>
-              <a href={\`mailto:\${settings.metadata.email}\`}>Email us</a>
+        return (
+          <div className="my-10">
+            <div className="my-8">
+              <NavMenu items={footer.metadata.items} />
             </div>
-            <div>
-              <a href={\`tel:\${settings.metadata.phone}\`}>Call us</a>
+            <div className="mb-8 flex gap-x-8 justify-center">
+              {settings.metadata.links.map((link: Link) => {
+                return (
+                  <a href={link.url} key={link.url} target="_blank" rel="noreferrer">
+                    <img
+                      className="h-[26px]"
+                      src={\`\${link.icon.imgix_url}?w=500&auto=format,compression\`}
+                      alt={link.company}
+                    />
+                  </a>
+                );
+              })}
+            </div>
+            <div className="flex gap-x-8 justify-center">
+              <div>
+                <a href={\`mailto:\${settings.metadata.email}\`}>Email us</a>
+              </div>
+              <div>
+                <a href={\`tel:\${settings.metadata.phone}\`}>Call us</a>
+              </div>
             </div>
           </div>
-        </div>
-      );
-    }
+        );
+      }
     \`\`\`
     `
 
