40909a75831ec29a0a1fa509f13e875c8a68cb81
Rajat Kaushik <rajatkaushiik@gmail.com>
2023-12-05 22:35:23 +0530

    fix: header image breaking

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <Providers>
             <div className="relative flex min-h-screen flex-col">
               <Navbar />
-              <div className="mx-auto flex w-screen grow flex-col items-center justify-center">
+              <div className="mx-auto flex w-screen grow flex-col items-center justify-center overflow-x-hidden">
                 {children}
               </div>
               <Footer />
