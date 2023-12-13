1fddf5b52c198ccd87a24a0ad94ca56e23b9d9f9
Tony Spiro <tspiro@tonyspiro.com>
2023-12-13 14:04:50 -0800

    edit: event updates

--- app/features/events/page.tsx
+++ app/features/events/page.tsx
@@ -4,6 +4,7 @@ import dedent from "dedent"
 import { fetchFeature } from "@/lib/cosmic"
 import { PackageManagers } from "../layout"
 import CodeSteps from "@/components/layouts/CodeSteps"
+import { EventCard, EventCardType } from "@/components/event-card"
 
 export default async function EventsPage({
   searchParams,
@@ -25,72 +26,13 @@ export default async function EventsPage({
   )
 }
 
-type EventType = {
-  title: string
-  slug: string
-  metadata: {
-    description: string
-    location: string
-    start_date: string
-    start_time: string
-    end_date: string
-    end_time: string
-    image: {
-      imgix_url: string
-    }
-  }
-}
-
-function Event({ event }: { event: EventType }) {
-  return (
-    <div className="relative mb-6 justify-start overflow-hidden rounded-xl bg-slate-100 p-8 dark:bg-slate-800 md:flex md:p-0">
-      <img
-        className="mx-auto h-auto w-[260px] rounded-full object-cover md:rounded-none"
-        src={`${event.metadata.image.imgix_url}?w=500&h=500&auto=format,compression&fit=facearea&facepad=3`}
-        alt={event.title}
-      />
-      <div className="w-full space-y-4 text-center md:p-8 md:text-left">
-        <div className="text-xl font-bold text-gray-700 dark:text-gray-200">
-          {event.title}
-        </div>
-        <div
-          className="relative z-10 text-lg font-medium text-slate-700 dark:text-slate-300"
-          dangerouslySetInnerHTML={{ __html: event.metadata.description }}
-        />
-        <div className="absolute bottom-4 font-medium">
-          <div className="mb-2 text-slate-700 dark:text-slate-300">
-            📆{" "}
-            {new Date(event.metadata.start_date).toLocaleDateString("en-us", {
-              weekday: "short",
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-            })}{" "}
-            {event.metadata.start_time} -{" "}
-            {new Date(event.metadata.end_date).toLocaleDateString("en-us", {
-              weekday: "short",
-              year: "numeric",
-              month: "short",
-              day: "numeric",
-            })}{" "}
-            {event.metadata.end_time}
-          </div>
-          <div className="text-slate-700 dark:text-slate-300">
-            📍 {event.metadata.location}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 async function Preview() {
-  const events = await fetchFeature<EventType>("events")
+  const events = await fetchFeature<EventCardType>("events")
 
   return (
     <div className="py-10">
-      {events?.map((event: EventType) => {
-        return <Event event={event} key={event.slug} />
+      {events?.map((event: EventCardType) => {
+        return <EventCard event={event} key={event.slug} />
       })}
     </div>
   )
@@ -103,19 +45,20 @@ function Code({ manager }: { manager: PackageManagers }) {
       import { cosmic } from "@/lib/cosmic";
 
       export async function Events() {
-        
+
         const { objects: events } = await cosmic.objects
           .find({
             type: "events",
           })
           .props("title,slug,metadata")
           .depth(1)
-
-        type EventType = {
+        
+        type EventCardType = {
           title: string
           slug: string
           metadata: {
             description: string
+            location: string
             start_date: string
             start_time: string
             end_date: string
@@ -125,8 +68,8 @@ function Code({ manager }: { manager: PackageManagers }) {
             }
           }
         }
-
-        function Event({ event }: { event: EventType }) {
+        
+        function EventCard({ event }: { event: EventCardType }) {
           return (
             <div className="relative mb-6 justify-start overflow-hidden rounded-xl bg-slate-100 p-8 dark:bg-slate-800 md:flex md:p-0">
               <img
@@ -143,24 +86,36 @@ function Code({ manager }: { manager: PackageManagers }) {
                   dangerouslySetInnerHTML={{ __html: event.metadata.description }}
                 />
                 <div className="absolute bottom-4 font-medium">
-                  <div className="text-slate-700 dark:text-slate-300">
-                    Starts {new Date(event.metadata.start_date).toLocaleDateString()},{" "}
-                    {event.metadata.start_time}
+                  <div className="mb-2 text-slate-700 dark:text-slate-300">
+                    📆{" "}
+                    {new Date(event.metadata.start_date).toLocaleDateString("en-us", {
+                      weekday: "short",
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                    })}{" "}
+                    {event.metadata.start_time} -{" "}
+                    {new Date(event.metadata.end_date).toLocaleDateString("en-us", {
+                      weekday: "short",
+                      year: "numeric",
+                      month: "short",
+                      day: "numeric",
+                    })}{" "}
+                    {event.metadata.end_time}
                   </div>
                   <div className="text-slate-700 dark:text-slate-300">
-                    Ends {new Date(event.metadata.end_date).toLocaleDateString()},{" "}
-                    {event.metadata.end_time}
+                    📍 {event.metadata.location}
                   </div>
                 </div>
               </div>
             </div>
           )
-        }
+        }        
         
         return (
           <>
-            {events?.map((event: EventType) => {
-              return <Event event={event} key={event.slug} />
+            {events?.map((event: EventCardType) => {
+              return <EventCard event={event} key={event.slug} />
             })}
           </>
         )
---
+++ components/event-card.tsx
@@ -0,0 +1,58 @@
+export type EventCardType = {
+  title: string
+  slug: string
+  metadata: {
+    description: string
+    location: string
+    start_date: string
+    start_time: string
+    end_date: string
+    end_time: string
+    image: {
+      imgix_url: string
+    }
+  }
+}
+
+export function EventCard({ event }: { event: EventCardType }) {
+  return (
+    <div className="relative mb-6 justify-start overflow-hidden rounded-xl bg-slate-100 p-8 dark:bg-slate-800 md:flex md:p-0">
+      <img
+        className="mx-auto h-auto w-[260px] rounded-full object-cover md:rounded-none"
+        src={`${event.metadata.image.imgix_url}?w=500&h=500&auto=format,compression&fit=facearea&facepad=3`}
+        alt={event.title}
+      />
+      <div className="w-full space-y-4 text-center md:p-8 md:text-left">
+        <div className="text-xl font-bold text-gray-700 dark:text-gray-200">
+          {event.title}
+        </div>
+        <div
+          className="relative z-10 text-lg font-medium text-slate-700 dark:text-slate-300"
+          dangerouslySetInnerHTML={{ __html: event.metadata.description }}
+        />
+        <div className="absolute bottom-4 font-medium">
+          <div className="mb-2 text-slate-700 dark:text-slate-300">
+            📆{" "}
+            {new Date(event.metadata.start_date).toLocaleDateString("en-us", {
+              weekday: "short",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}{" "}
+            {event.metadata.start_time} -{" "}
+            {new Date(event.metadata.end_date).toLocaleDateString("en-us", {
+              weekday: "short",
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })}{" "}
+            {event.metadata.end_time}
+          </div>
+          <div className="text-slate-700 dark:text-slate-300">
+            📍 {event.metadata.location}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
