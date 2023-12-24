4ebff191526727113894c18a26009db7b387a7ea
Tony Spiro <tspiro@tonyspiro.com>
2023-12-24 13:51:50 -0800

    fix: api comment code

--- app/features/comments/page.tsx
+++ app/features/comments/page.tsx
@@ -89,8 +89,8 @@ function Code() {
     \`\`\`ts
       // app/api/comments/route.ts
       import { type NextRequest } from "next/server";
-      import { cosmic } from "@/lib/cosmic";
-
+      import { cosmic } from "@/cosmic/client";
+      
       export async function POST(request: NextRequest) {
         const res = await request.json();
         const data = await cosmic.objects.insertOne(res.comment);
