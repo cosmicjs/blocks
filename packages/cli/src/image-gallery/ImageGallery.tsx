4cd9454f1258803ae7f5dfa2f8aee841f1487894
Tony Spiro <tspiro@tonyspiro.com>
2023-12-29 14:23:34 -0800

    add: className to image gallery

--- packages/cli/src/image-gallery/ImageGallery.tsx
+++ packages/cli/src/image-gallery/ImageGallery.tsx
@@ -1,10 +1,19 @@
 import { ImageGalleryClient } from "./ImageGalleryClient"
 import { cosmic } from "@/cosmic/client"
 
-export async function ImageGallery({ query }: { query: any }) {
+export async function ImageGallery({
+  query,
+  className,
+}: {
+  query: any
+  className?: string
+}) {
   const { object: page } = await cosmic.objects
     .findOne(query)
     .props("slug,title,metadata")
     .depth(1)
-  return <ImageGalleryClient items={page.metadata.gallery} />
+  if (!page.metadata.gallery?.length) return <></>
+  return (
+    <ImageGalleryClient items={page.metadata.gallery} className={className} />
+  )
 }
