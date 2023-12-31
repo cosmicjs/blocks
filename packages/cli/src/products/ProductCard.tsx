357dccc9b9284eddad33c935289403732c323455
Tony Spiro <tspiro@tonyspiro.com>
2023-12-30 19:13:26 -0800

    edit: product styles

--- packages/cli/src/products/ProductCard.tsx
+++ packages/cli/src/products/ProductCard.tsx
@@ -31,17 +31,17 @@ export function ProductCard({
         <img
           src={`${product.metadata.image.imgix_url}?w=1200&auto=format,compression`}
           alt={product.title}
-          className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+          className="h-full w-full border border-zinc-100 object-cover object-center dark:border-zinc-800 lg:h-full lg:w-full"
         />
       </div>
-      <div className="mt-4 flex justify-between">
+      <div className="mt-2 flex justify-between">
         <div>
-          <h3 className="text-sm text-gray-700">
+          <h3 className="text-sm text-zinc-700 dark:text-zinc-300">
             <span aria-hidden="true" className="absolute inset-0"></span>
             {product.title}
           </h3>
         </div>
-        <p className="text-sm font-medium text-gray-900">
+        <p className="text-sm font-medium text-zinc-900 dark:text-zinc-50">
           ${product.metadata.price.toLocaleString("en-US")}
         </p>
       </div>
