ea81509966caaa297698062d862891f07d6bccbc
Tony Spiro <tspiro@tonyspiro.com>
2024-01-01 16:31:59 -0800

    edit: pages style

--- packages/cli/src/pages/Page.tsx
+++ packages/cli/src/pages/Page.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/cosmic/utils"
 import { buttonVariants } from "@/cosmic/elements/Button"
 import { Section } from "./PageSection"
 import { cosmic } from "@/cosmic/client"
+import Link from "next/link"
 
 export async function Page({
   query,
@@ -18,51 +19,70 @@ export async function Page({
 
   return (
     <div className={className}>
-      <div className="pb-4 pt-20">
-        <h1 className="text-center text-4xl font-bold">{page.metadata.h1}</h1>
-      </div>
-      <div className="m-auto max-w-3xl pb-8">
-        <div className="text-center text-xl">{page.metadata.subheadline}</div>
-      </div>
-      <div className="m-auto max-w-3xl pb-20">
-        <div className="text-center text-xl">
-          <a
-            className={cn(
-              "mr-4",
-              buttonVariants({
-                variant: "default",
-              })
-            )}
-            href="https://www.cosmicjs.com"
-          >
-            Get started free
-          </a>
-          <a
-            className={buttonVariants({
-              variant: "secondary",
-            })}
-            href="https://www.cosmicjs.com/contact"
-          >
-            Contact us
-          </a>
+      <div className="mx-auto flex w-full max-w-6xl flex-col-reverse justify-between pb-16 text-zinc-950 dark:text-zinc-50 md:flex-row md:gap-12">
+        <div className="flex w-full flex-col items-start justify-start md:w-1/2">
+          <div className="pb-4 pt-4 md:pt-20">
+            <h1 className="font-display text-4xl tracking-tight md:text-8xl">
+              {page.metadata.h1}
+            </h1>
+          </div>
+          <div className="m-auto pb-8">
+            <div className="text-xl text-zinc-700 dark:text-zinc-300">
+              {page.metadata.subheadline}
+            </div>
+          </div>
+          <div className="w-full md:pb-20">
+            <div className="flex w-full gap-4 md:w-max">
+              <Link
+                className={cn(
+                  "w-full md:w-max",
+                  buttonVariants({
+                    variant: "default",
+                  })
+                )}
+                href="https://www.cosmicjs.com"
+              >
+                Get started free
+              </Link>
+              <Link
+                className={cn(
+                  "w-full md:w-max",
+                  buttonVariants({
+                    variant: "secondary",
+                  })
+                )}
+                href="https://www.cosmicjs.com/contact"
+              >
+                Contact us
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="my-auto w-full px-4 md:w-1/2">
+          <img
+            src={`${page.metadata.image.imgix_url}?w=1600&auto=format,compression`}
+            alt={page.title}
+            className="w-full dark:hidden"
+          />
+          <img
+            src={`${page.metadata.dark_image.imgix_url}?w=1600&auto=format,compression`}
+            alt={page.title}
+            className="hidden w-full dark:block"
+          />
         </div>
       </div>
-      <div className="m-auto mb-10 w-full max-w-[1100px] px-4">
-        <img
-          src={`${page.metadata.image.imgix_url}?w=3000&auto=format,compression`}
-          alt={page.title}
-          className="w-full"
-        />
-      </div>
-      <section className="container m-auto grid items-center py-10">
-        <div className="relative m-auto flex max-w-3xl flex-col items-start gap-2">
-          <div
+      <section className="-m-4 grid items-center bg-zinc-50 p-4 py-10 dark:bg-zinc-900">
+        <div className="relative m-auto flex max-w-6xl flex-col items-start gap-2">
+          <h2 className="font-display m-auto max-w-[800px] pt-8 text-center text-3xl text-zinc-900 dark:text-zinc-100 md:text-6xl">
+            {page.metadata.section_title}
+          </h2>
+          <p
             dangerouslySetInnerHTML={{ __html: page.metadata.content }}
-            className="m-auto mb-16 max-w-[800px]"
+            className="m-auto mb-16 max-w-[800px] text-center text-zinc-700 dark:text-zinc-300"
           />
           <div className="grid gap-y-28">
             {page.metadata.sections.map((section: any) => {
-              return <Section key={section.header} section={section} />
+              return <Section key={section.heading} section={section} />
             })}
           </div>
         </div>
