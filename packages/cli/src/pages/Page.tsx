c59cdf5a65bc27d13d62718179303ca46358b992
Tony Spiro <tspiro@tonyspiro.com>
2024-01-01 19:38:20 -0800

    fix: page screen overflow issue

--- packages/cli/src/pages/Page.tsx
+++ packages/cli/src/pages/Page.tsx
@@ -71,7 +71,7 @@ export async function Page({
           />
         </div>
       </div>
-      <section className="-m-4 grid items-center bg-zinc-50 p-4 py-10 dark:bg-zinc-900">
+      <section className="grid items-center bg-zinc-50 p-4 py-10 dark:bg-zinc-900">
         <div className="relative m-auto flex max-w-6xl flex-col items-start gap-2">
           <h2 className="font-display m-auto max-w-[800px] pt-8 text-center text-3xl text-zinc-900 dark:text-zinc-100 md:text-6xl">
             {page.metadata.section_title}
