8478186496c8b497f639cc97772318a9fd63ff54
Tony Spiro <tspiro@tonyspiro.com>
2024-01-18 14:13:08 -0800

    edit: nav menu justify center

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -12,19 +12,17 @@ export async function NavMenu({ query }: { query: any }) {
   return (
     <>
       {/* Desktop */}
-      <div className="hidden md:block">
-        <div>
-          {nav.metadata.items.map((item: ItemType) => {
-            return (
-              <div
-                key={item.title}
-                className="group inline-flex h-10 w-full cursor-pointer items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-zinc-100 data-[state=open]:bg-zinc-100 dark:text-zinc-50 dark:hover:bg-zinc-800 dark:data-[state=active]:bg-zinc-900 dark:data-[state=open]:bg-zinc-900 md:w-max"
-              >
-                {item.title}
-              </div>
-            )
-          })}
-        </div>
+      <div className="hidden justify-center md:flex">
+        {nav.metadata.items.map((item: ItemType) => {
+          return (
+            <div
+              key={item.title}
+              className="group inline-flex h-10 w-full cursor-pointer items-center justify-center rounded-md bg-transparent px-4 py-2 text-sm font-medium text-zinc-900 transition-colors hover:bg-zinc-100 focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-zinc-100 data-[state=open]:bg-zinc-100 dark:text-zinc-50 dark:hover:bg-zinc-800 dark:data-[state=active]:bg-zinc-900 dark:data-[state=open]:bg-zinc-900 md:w-max"
+            >
+              {item.title}
+            </div>
+          )
+        })}
       </div>
       {/* Mobile */}
       <MobileNav items={nav.metadata.items} />
