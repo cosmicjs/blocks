a4d3d17069a32cb4eae6f2349755f037451382ce
Tony Spiro <tspiro@tonyspiro.com>
2024-01-28 15:59:55 -0800

    edit: noWrap option for products

--- packages/cli/src/products/ProductList.tsx
+++ packages/cli/src/products/ProductList.tsx
@@ -1,6 +1,16 @@
 import { cosmic } from "@/cosmic/client"
 import { ProductCard, ProductType } from "./ProductCard"
 
+function Products({ products }: { products: ProductType[] }) {
+  return (
+    <>
+      {products.map((product: ProductType) => {
+        return <ProductCard key={product.id} product={product} />
+      })}
+    </>
+  )
+}
+
 export async function ProductList({
   query,
   sort,
@@ -8,6 +18,7 @@ export async function ProductList({
   skip,
   className,
   status,
+  noWrap = false,
 }: {
   query: any
   sort?: string
@@ -15,6 +26,7 @@ export async function ProductList({
   skip?: number
   className?: string
   status?: "draft" | "published" | "any"
+  noWrap?: boolean
 }) {
   const { objects: products } = await cosmic.objects
     .find(query)
@@ -24,7 +36,7 @@ export async function ProductList({
     .limit(limit ? limit : 100)
     .skip(skip ? skip : 0)
     .status(status ? status : "published")
-
+  if (noWrap) return <Products products={products} />
   return (
     <div
       className={`m-auto grid max-w-[1200px] grid-cols-1 gap-4 px-4 md:grid-cols-3 lg:grid-cols-4 ${className}`}
