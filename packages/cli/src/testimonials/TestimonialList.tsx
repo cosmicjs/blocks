e1c2ec9dd41cae15e763e6a8c98000781c7ce7b5
Tony Spiro <tspiro@tonyspiro.com>
2024-01-29 15:09:43 -0800

    edit: Testimonials to TestimonialList

--- packages/cli/src/testimonials/TestimonialList.tsx
+++ packages/cli/src/testimonials/TestimonialList.tsx
@@ -1,13 +1,24 @@
 import { cosmic } from "@/cosmic/client"
 import { Testimonial, TestimonialType } from "./Testimonial"
 
-export async function Testimonials({
+function Testimonials({ testimonials }: { testimonials: TestimonialType[] }) {
+  return (
+    <>
+      {testimonials?.map((testimonial: TestimonialType) => {
+        return <Testimonial testimonial={testimonial} key={testimonial.slug} />
+      })}
+    </>
+  )
+}
+
+export async function TestimonialList({
   query,
   sort,
   limit,
   skip,
   className,
   status,
+  noWrap = false,
 }: {
   query: any
   sort?: string
@@ -15,6 +26,7 @@ export async function Testimonials({
   skip?: number
   className?: string
   status?: "draft" | "published" | "any"
+  noWrap?: boolean
 }) {
   const { objects: testimonials } = await cosmic.objects
     .find(query)
@@ -24,11 +36,10 @@ export async function Testimonials({
     .limit(limit ? limit : 100)
     .skip(skip ? skip : 0)
     .status(status ? status : "published")
+  if (noWrap) return <Testimonials testimonials={testimonials} />
   return (
     <div className={`m-auto max-w-[800px] p-4 ${className}`}>
-      {testimonials?.map((testimonial: TestimonialType) => {
-        return <Testimonial testimonial={testimonial} key={testimonial.slug} />
-      })}
+      <Testimonials testimonials={testimonials} />
     </div>
   )
 }
