5afb68d1dd2c186305507b491808efd1131e789d
Tony Spiro <tspiro@tonyspiro.com>
2024-02-21 15:42:19 -0800

    Update page.tsx

--- app/blocks/contact-form/page.tsx
+++ app/blocks/contact-form/page.tsx
@@ -64,7 +64,7 @@ function Code() {
     \`\`\`jsx
     // app/contact/page.tsx
     import { ContactForm } from "@/cosmic/blocks/contact-form/ContactForm";
-    export default async function HomePage() {
+    export default async function ContactPage() {
       return (
         <>
           <ContactForm className="w-full max-w-[500px] m-auto my-10" />
