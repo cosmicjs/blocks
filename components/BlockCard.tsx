34a5306411f0ab02924a1eb2cd66f4734db16671
Tony Spiro <tspiro@tonyspiro.com>
2024-11-08 11:21:06 -0800

    edit: layout

--- components/BlockCard.tsx
+++ components/BlockCard.tsx
@@ -21,7 +21,7 @@ export function BlockCard({ feature }: { feature: BlockData }) {
 
   return (
     <Link
-      className="flex rounded-lg border border-gray-200 bg-white p-5 text-gray-800 transition-all duration-100 hover:border-gray-300 dark:border-dark-gray-200 dark:bg-gray-800 dark:text-dark-gray-800 hover:dark:bg-gray-700"
+      className="flex rounded-lg border border-gray-200 bg-white p-4 text-gray-800 transition-all duration-100 hover:border-gray-300 dark:border-dark-gray-200 dark:bg-gray-800 dark:text-dark-gray-800 hover:dark:bg-gray-700"
       href={feature?.preview_link}
     >
       {feature.icon("h-10 w-10 mr-6")}
